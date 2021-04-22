ff404c79c51f5b5fee7974eb7d83db1f55103735
Cesarzg <76677457@certus.edu.pe>
2021-04-22 18:06:01 -0500

    Nuevo repositorio

---
+++ Hola mundo.py
@@ -0,0 +1,45 @@
+print('Hola mundo')
+edad=2
+print(edad)
+print('tengo', edad,'años')
+base=10
+
+#suma
+a=5
+b=6
+suma=a+b
+print ('La suma es: ', suma)
+#resta
+resta=a-b
+print('La resta es: ',resta)
+#multiplicacion
+multiplicacion=a*b
+print('La multiplicacion es: ',multiplicacion)
+#division
+division=a /b
+print('La division es: ',division)
+#potencia
+potencia=a**b
+print('La potencia es: ',potencia)
+
+#operaciones semi-complejas
+#Realizar la siguiene operacion en python ((4+5)*3)/2
+x=4
+y=5
+w=3
+z=2
+#resultado
+Resultado =((x+y)*w)/z 
+print('El resultado es: ', Resultado)
+
+
+str1='a'
+str2='b'
+resultado= str1+str2
+print('El resultado esa : ', resultado)
+print(resultado*5)
+caracteres=len('Hola mundo')
+print(caracteres)
+name=input('Cual es tu nombre')
+print('hola', name)
+print('hola'+name)
